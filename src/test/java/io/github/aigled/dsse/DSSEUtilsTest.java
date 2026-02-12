25cda3b910fc13d5b4bf53725387a9546bcdc98e
Dorian Aigle <12817906+aigled@users.noreply.github.com>
2026-02-12 18:44:56 +0100

    docs: add copyright headers to source files

--- src/test/java/io/github/aigled/dsse/DSSEUtilsTest.java
+++ src/test/java/io/github/aigled/dsse/DSSEUtilsTest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2026 Dorian AIGLE
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 package io.github.aigled.dsse;
 
 import org.junit.jupiter.api.Test;
